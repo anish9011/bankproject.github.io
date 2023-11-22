c869a7bbcb185d3620378f61b4911f28ce862e0b
Anish Shetty <129471742+anish9011@users.noreply.github.com>
2023-11-22 22:05:22 +0530

    Add files via upload

---
+++ aniscripts.js
@@ -0,0 +1,8 @@
+function myFunction1() {
+    document.getElementById("demo").innerHTML = "<h1>Hello its changed</h1>";
+  }
+
+  function myFunction() {
+    document.getElementById("btn1").innerHTML = "<h1>hello anish</h1>";
+  }
+
